da577e2cfd0ceb223f1f170e268fb75f58c4f062
Hug3O <131569471+hugo529100@users.noreply.github.com>
2025-05-15 13:49:54 +0800

    Delete plugin.js

--- plugin.js
+++
@@ -1,5 +0,0 @@
-exports.version = 3.2
-exports.description = "To modifying the HFS3 interface to make it more suitable for daily management and browsing of images and videos."
-exports.apiRequired = 1
-exports.repo = "Hug3O/hfs-Orange-theme"
-exports.frontend_css = 'style.css'
