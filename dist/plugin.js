083226f798c1aa224a1ed69082b839c29affe823
Hug3O <131569471+hugo529100@users.noreply.github.com>
2025-05-17 15:14:45 +0800

    Add files via upload

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,4 +1,4 @@
-exports.version = 3.2
+exports.version = 3.3
 exports.description = "To modifying the HFS3 interface to make it more suitable for daily management and browsing of images and videos."
 exports.apiRequired = 1
 exports.repo = "Hug3O/hfs-Orange-theme"
